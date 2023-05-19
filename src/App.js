1f6bd3294912ddcff9eeecdbd94c523b5dea3e75
janahasan <99503558+janahasa@users.noreply.github.com>
2023-05-19 21:36:29 +0300

    Add files via upload
    this is the project until now,
    and I will split the code into branches soon,
    If you have any comments, please let me know to take them into consideration.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Tv from "./pages/Tv";
+import BookMarks from "./pages/BookMarks";
+import { BookMarkedContext } from "./context/BookMarkedContext";
+import ShowDetails from "./pages/ShowDetails";
+import ShowVideoDetails from "./components/ShowVideoDetails";
+import ShowTVDetails from "./components/ShowTVDetails";
+import NotFoundPage from "./components/NotFoundPage";
+import Footer from "./components/Footer";
+import Login from "./components/login";
+import SignUp from "./components/signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/Entertainment-web-app">
+        <Navbar/>
+        <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} /> 
+        <Route path="*" element={<NotFoundPage />}/>
+        <Route path="/movies" element={<Movies />}/>
+        <Route path="/tvSeries" element={<Tv />}/>
+        <Route path="/bookMarks" element={<BookMarks />}/>
+        <Route path="/movies/:id" element={<ShowVideoDetails />}/>
+        <Route path="/series/:id" element={<ShowTVDetails />}/>
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
