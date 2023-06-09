a2612890231a812383ff02e853c2b06560d2f896
janahasan <99503558+janahasa@users.noreply.github.com>
2023-06-09 11:22:28 +0300

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword, sendEmailVerification, onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../components/config/firebase-config';
